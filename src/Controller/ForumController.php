5dfc914eb52f08033addceabd3b1ce2845027f73
Arnaud Kameni <bekaman100@gmail.com>
2023-11-06 06:08:43 +0100

    last update

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\CategoryForum;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\BrowserKit\Request;
+use App\Repository\CategoryForumRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ForumController extends AbstractController
 {
@@ -12,6 +16,33 @@ class ForumController extends AbstractController
     public function index(): Response
     {
         return $this->render('forum/index.html.twig', [
+            'controller_name' => 'ForumController',
+        ]);
+    }
+    
+    /*
+    * CATEGORY CRUD
+    */
+   
+    #[Route('/forum/category', name: 'app_category')]
+    public function category(CategoryForumRepository $categoryForumRepository): Response
+    {
+        $categoriesForum = $categoryForumRepository->findAll();
+
+        return $this->render('forum/category.html.twig', [
+            'categoriesForum' => $categoriesForum,
+        ]);
+    }
+    
+     // Create a new category
+
+    #[Route('/forum/category/new', name: 'app_new')]
+    #[Route('/forum/category/new', name: 'app_edit')]
+    public function new_edit(CategoryForum $categoryForum = null, Request $Resquest, EntityManagerInterface $entityManager): Response
+    {
+
+        
+        return $this->render('forum/new_edit.html.twig', [
             'controller_name' => 'ForumController',
         ]);
     }
